d33967bc30ea7ec08a1b5b9496c925f950c202ba
Vladimir Yunev <vyunev@live.ru>
2015-08-07 10:48:31 +0300

    First Commit

---
+++ ControllerDemo/src/ControllerDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ControllerDemo.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly string _time;
+
+		public HomeController()
+		{
+			this._time = DateTime.Now.ToString("G");
+		}
+
+		// GET: /Home/
+		public IActionResult Index() 
+		{
+			return this.Content("Hello ASP.NET MVC 6");
+		}
+
+		// GET: /Home/Echo/
+		public IActionResult Echo(string message)
+		{
+			return this.Content($"{this._time} > {message}");
+		}
+
+
+		// GET: /Home/EchoAsync/
+		public async Task<IActionResult> EchoAsync(string message)
+		{
+			await Task.Delay(100);
+			return this.Content($"{this._time} > {message}");
+		}
+	}
+}
---
+++ ControllerDemo/src/ControllerDemo/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace ControllerDemo
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+			services.AddMvc();
+		}
+
+        public void Configure(IApplicationBuilder app)
+        {
+			app.UseMvc(routes => {
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action}/{id?}",
+					defaults: new { controller = "Home", action = "Index" });
+			});
+		}
+    }
+}
